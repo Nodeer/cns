39d53116cb6031645ae10bbe9037a17c99cffaa0
Greg Pechiro <gregpechiro@yahoo.com>
2016-04-14 16:46:39 -0400

    added open in new tab

--- static/datatables/custom.js
+++ static/datatables/custom.js
@@ -1,3 +1,7 @@
 $(document).on('click', 'tr.clickable', function() {
+    if (this.getAttribute('data-target') === '_blank') {
+        window.open(this.getAttribute('data-url'));
+        return
+    }
     window.location.href = this.getAttribute('data-url');
 });
