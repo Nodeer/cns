c01aad7f0ef249099d4980607596bab97b9ff630
gabewitmer <gabenwitmer@gmail.com>
2016-06-10 14:57:52 -0400

    created dqf200

--- temp.go
+++ temp.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	mx.AddRoutes(test, dqf450)
+	mx.AddRoutes(test, dqf450, dqf200, dqf250)
 }
 
 var test = web.Route{"GET", "/test", func(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +16,12 @@ var test = web.Route{"GET", "/test", func(w http.ResponseWriter, r *http.Request
 
 var dqf450 = web.Route{"GET", "/450", func(w http.ResponseWriter, r *http.Request) {
 	tc.Render(w, r, "dqf-450.tmpl", web.Model{})
+}}
+
+var dqf200 = web.Route{"GET", "/200", func(w http.ResponseWriter, r *http.Request) {
+	tc.Render(w, r, "dqf-200.tmpl", web.Model{})
+}}
+
+var dqf250 = web.Route{"GET", "/250", func(w http.ResponseWriter, r *http.Request) {
+	tc.Render(w, r, "dqf-250.tmpl", web.Model{})
 }}
