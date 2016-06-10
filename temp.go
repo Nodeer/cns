4d803450f2b54b72e25f8561f9419329ec5b80af
gabewitmer <gabenwitmer@gmail.com>
2016-06-10 13:54:30 -0400

    created dqf450

--- temp.go
+++ temp.go
@@ -7,9 +7,13 @@ import (
 )
 
 func init() {
-	mx.AddRoutes(test)
+	mx.AddRoutes(test, dqf450)
 }
 
 var test = web.Route{"GET", "/test", func(w http.ResponseWriter, r *http.Request) {
 	tc.Render(w, r, "test.tmpl", web.Model{})
+}}
+
+var dqf450 = web.Route{"GET", "/450", func(w http.ResponseWriter, r *http.Request) {
+	tc.Render(w, r, "dqf-450.tmpl", web.Model{})
 }}
